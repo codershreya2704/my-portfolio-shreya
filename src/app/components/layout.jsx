4b9be7b8a3c7eaea69ffac2d01115a4faae63af4
shreyatambad <52215220+codershreya2704@users.noreply.github.com>
2025-11-13 23:34:05 +0530

    Update layout.jsx

--- src/app/components/layout.jsx
+++ src/app/components/layout.jsx
@@ -3,7 +3,6 @@ import { ThemeProvider, CssBaseline } from '@mui/material';
 import theme from '../theme';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import './globals.css';
 
 export default function RootLayout({ children }) {
   return (
