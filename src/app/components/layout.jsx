3dd692040ee92e153395ec95f248bf9cf7b110ee
shreyatambad <52215220+codershreya2704@users.noreply.github.com>
2025-11-13 23:36:49 +0530

    Update layout.jsx

--- src/app/components/layout.jsx
+++ src/app/components/layout.jsx
@@ -1,20 +1,20 @@
-'use client';
-import { ThemeProvider, CssBaseline } from '@mui/material';
-import theme from '../theme';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+// 'use client';
+// import { ThemeProvider, CssBaseline } from '@mui/material';
+// import theme from '../theme';
+// import Navbar from './components/Navbar';
+// import Footer from './components/Footer';
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Navbar />
-          {children}
-          <Footer />
-        </ThemeProvider>
-      </body>
-    </html>
-  );
-}
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body>
+//         <ThemeProvider theme={theme}>
+//           <CssBaseline />
+//           <Navbar />
+//           {children}
+//           <Footer />
+//         </ThemeProvider>
+//       </body>
+//     </html>
+//   );
+// }
